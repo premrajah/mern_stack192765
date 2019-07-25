c538c06b3a7fa130c9afc88d0b9258ab4b008ffa
Prem Rajah <prajah@ctlondon.com>
2019-07-26 00:08:08 +0100

    Added git ignore file added .env file and added mongo connection string edited server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());                // use cors middle ware
-app.use(express().json());      // instead of bod parser 
+app.use(cors());              // use cors middle ware
+app.use(express.json());      // instead of body parser now included in express
 
+const uri = process.env.ATLAS_URI;   // URI from Mongodb atlas
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }); // 
+
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully.');
+});
 
 
 
